f9adf3d482e1a0cf1c95a0a1d53714691c5ed76a
Linus Kinzi-paludan <skinzip@gmail.com>
2018-04-12 19:11:16 +0200

    Added the rest of Linus - test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,6 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                    <a href="{{ url('/linus') }}">Linus</a>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
